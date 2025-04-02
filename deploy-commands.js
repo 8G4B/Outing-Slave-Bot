c29e2c5464f1b4ef5a45e3eb8d7311abd63c79ec
KIM TAEEUN <snowykte0426@naver.com>
2025-04-03 00:14:24 +0900

    슬래시 명령어 등록 스크립트 추가: 채널 설정, 비활성화, 임베드 메시지 테스트 기능 포함

---
+++ deploy-commands.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { Client, GatewayIntentBits, REST, Routes } = require('discord.js');
+const { token, clientId } = require('./config.json');
+
+const commands = [
+  new SlashCommandBuilder().setName('setchannel').setDescription('설정할 채널 ID를 입력합니다.').addStringOption(option =>
+    option.setName('channel_id').setDescription('채널 ID').setRequired(true)),
+  new SlashCommandBuilder().setName('disable').setDescription('알림을 비활성화합니다.'),
+  new SlashCommandBuilder().setName('testembed').setDescription('임베드 메시지를 테스트로 전송합니다.')
+]
+  .map(command => command.toJSON());
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+(async () => {
+  try {
+    console.log('슬래시 명령어 등록 중...');
+
+    await rest.put(
+      Routes.applicationCommands(clientId),
+      { body: commands },
+    );
+
+    console.log('슬래시 명령어 등록 완료!');
+  } catch (error) {
+    console.error(error);
+  }
+})();
