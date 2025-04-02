a533101f794f4afe545d12c521d3aca63520f3f1
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-04-03 08:32:03 +0900

    deploy-commands.js 수정: 코드 스타일 개선 및 일관성 있는 문자열 사용

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,27 +1,31 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { Client, GatewayIntentBits, REST, Routes } = require('discord.js');
-const { token, clientId } = require('./config.json');
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { REST, Routes } = require("discord.js");
+const { token, clientId } = require("./config.json");
 
 const commands = [
-  new SlashCommandBuilder().setName('setchannel').setDescription('설정할 채널 ID를 입력합니다.').addStringOption(option =>
-    option.setName('channel_id').setDescription('채널 ID').setRequired(true)),
-  new SlashCommandBuilder().setName('disable').setDescription('알림을 비활성화합니다.'),
-  new SlashCommandBuilder().setName('testembed').setDescription('임베드 메시지를 테스트로 전송합니다.')
-]
-  .map(command => command.toJSON());
+  new SlashCommandBuilder()
+    .setName("setchannel")
+    .setDescription("설정할 채널 ID를 입력합니다.")
+    .addStringOption((option) =>
+      option.setName("channel_id").setDescription("채널 ID").setRequired(true)
+    ),
+  new SlashCommandBuilder()
+    .setName("disable")
+    .setDescription("알림을 비활성화합니다."),
+  new SlashCommandBuilder()
+    .setName("testembed")
+    .setDescription("임베드 메시지를 테스트로 전송합니다."),
+].map((command) => command.toJSON());
 
-const rest = new REST({ version: '10' }).setToken(token);
+const rest = new REST({ version: "10" }).setToken(token);
 
 (async () => {
   try {
-    console.log('슬래시 명령어 등록 중...');
+    console.log("슬래시 명령어 등록 중...");
 
-    await rest.put(
-      Routes.applicationCommands(clientId),
-      { body: commands },
-    );
+    await rest.put(Routes.applicationCommands(clientId), { body: commands });
 
-    console.log('슬래시 명령어 등록 완료!');
+    console.log("슬래시 명령어 등록 완료!");
   } catch (error) {
     console.error(error);
   }
