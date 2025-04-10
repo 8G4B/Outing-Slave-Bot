51b5feb1a928ef5eee3b5a44365b17d8c8f54dbb
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-04-10 16:43:42 +0900

    코드 스타일 개선: 일관된 따옴표 사용 및 코드 포맷 정리

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,27 +1,31 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { Client, GatewayIntentBits, REST, Routes } = require('discord.js');
-const { token, clientId } = require('./config.json');
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { REST, Routes } = require("discord.js"); // Client, GatewayIntentBits,
+const { token, clientId } = require("./config.json");
 
 const commands = [
-  new SlashCommandBuilder().setName('채널설정').setDescription('알림을 보낼 채널 ID를 설정합니다.').addStringOption(option =>
-    option.setName('채널_id').setDescription('채널 ID').setRequired(true)),
-  new SlashCommandBuilder().setName('알림비활성화').setDescription('알림을 비활성화합니다.'),
-  new SlashCommandBuilder().setName('임베드테스트').setDescription('임베드 메시지를 테스트로 전송합니다.')
-]
-  .map(command => command.toJSON());
+  new SlashCommandBuilder()
+    .setName("채널설정")
+    .setDescription("알림을 보낼 채널 ID를 설정합니다.")
+    .addStringOption((option) =>
+      option.setName("채널_id").setDescription("채널 ID").setRequired(true)
+    ),
+  new SlashCommandBuilder()
+    .setName("알림비활성화")
+    .setDescription("알림을 비활성화합니다."),
+  new SlashCommandBuilder()
+    .setName("임베드테스트")
+    .setDescription("임베드 메시지를 테스트로 전송합니다."),
+].map((command) => command.toJSON());
 
-const rest = new REST({ version: '10' }).setToken(token);
+const rest = new REST({ version: "10" }).setToken(token);
 
 (async () => {
   try {
-    console.log('슬래시 명령어 등록 중...');
+    console.log("슬래시 명령어 등록 중...");
 
-    await rest.put(
-      Routes.applicationCommands(clientId),
-      { body: commands },
-    );
+    await rest.put(Routes.applicationCommands(clientId), { body: commands });
 
-    console.log('슬래시 명령어 등록 완료!');
+    console.log("슬래시 명령어 등록 완료!");
   } catch (error) {
     console.error(error);
   }
